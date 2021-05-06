a433052ba62ac47e7576b19f5f315b4e50b5e8ab
Rahul Haridas <48543567+rahulharidas1@users.noreply.github.com>
2021-05-06 21:04:08 +0530

    added input fields and markdown area

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,10 @@ import "./styles.css";
 const Main = () => {
   return (
     <div className="main-app-area">
-      <div className="note-edit-area">Write contents here!</div>
+      <div className="note-edit-area">
+        <input type="text" className="note-header-input" />
+        <textarea className="note-content-input" />
+      </div>
       <div className="markdown-area">
         This is where the markdown will be displayed!
       </div>
