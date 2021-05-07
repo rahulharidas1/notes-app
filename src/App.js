98f86c2fab2492f0edd6f0de9c7bfc396728946f
Rahul Haridas <48543567+rahulharidas1@users.noreply.github.com>
2021-05-07 20:26:18 +0530

    Installed uuid package and added 'notes' state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import "./styles.css";
 
 import Sidebar from "./Sidebar";
 import Main from "./Main";
+import { useState } from "react";
+import uuid from "react-uuid";
 
 export default function App() {
+  const [notes, setNotes] = useState([]);
+  const onAddCallBack = () => {
+    var noteContent = {
+      id: uuid(),
+      title: "Untitled",
+      body: "Random Text!",
+      lastModified: Date.now()
+    };
+    setNotes([noteContent, ...notes]);
+  };
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddCallBack={onAddCallBack} notes={notes} />
       <Main />
     </div>
   );
