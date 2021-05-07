47b6750dfbb50556efd22d27b7b4d940270f439a
Rahul Haridas <48543567+rahulharidas1@users.noreply.github.com>
2021-05-07 20:34:04 +0530

    feat: added functionality to 'add' button

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ import uuid from "react-uuid";
 
 export default function App() {
   const [notes, setNotes] = useState([]);
-  const onAddCallBack = () => {
+  const onAddCallback = () => {
     var noteContent = {
       id: uuid(),
       title: "Untitled",
       body: "Random Text!",
       lastModified: Date.now()
     };
+    console.log("Hey!");
     setNotes([noteContent, ...notes]);
   };
   return (
     <div className="App">
-      <Sidebar onAddCallBack={onAddCallBack} notes={notes} />
+      <Sidebar onAddCallback={onAddCallback} notes={notes} />
       <Main />
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,30 @@
 import "./styles.css";
 
-const Sidebar = () => {
+const Sidebar = ({ onAddCallback, notes }) => {
   return (
     <>
       <div className="sidebar-content">
         <div className="sidebar-header">
           <h1>Notes</h1>
-          <button className="button-style">Add</button>
+          <button className="button-style" onClick={onAddCallback}>
+            Add
+          </button>
         </div>
         <div className="sidebar-notes">
-          <div className="sidebar-note">
-            <div className="sidebar-note-header">
-              <h4>Note header</h4>
-              <button className="button-style">Delete</button>
-            </div>
-            <div className="note-content">this is a random note.</div>
-            <div className="last-modified">Last Modified: date</div>
-          </div>
+          {notes.map((note, id) => {
+            return (
+              <div className="sidebar-note" key={id}>
+                <div className="sidebar-note-header">
+                  <h4>{note.title}</h4>
+                  <button className="button-style">Delete</button>
+                </div>
+                <div className="note-content">{note.body}</div>
+                <div className="last-modified">
+                  Last Modified: {note.lastModified}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
